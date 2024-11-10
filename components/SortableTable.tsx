16cd923667212a891d41fea7ef4d4179cd8d9242
Elias Renawi <elias.renawi@bizi.co.il>
2024-11-10 13:56:47 +0200

    add table

--- components/SortableTable.tsx
+++ components/SortableTable.tsx
@@ -89,6 +89,7 @@ export default function SortableTable() {
 
   return (
     <div className="rounded-md border">
+      sortedtable
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
